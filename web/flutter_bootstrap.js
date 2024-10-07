432073a7ad537ceeaf12ff9ffbd1521fa72ca2cf
LEE SEONGHA <lsh1230@istn.co.kr>
2024-10-07 16:22:41 +0900

    d

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"66d397dff87acec3813750e41ea2a293f3f003
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4238275736"
+    serviceWorkerVersion: "1559077290"
   }
 });
